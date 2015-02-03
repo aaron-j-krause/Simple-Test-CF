13e68120d84d6bc064d2887cd16d5456a7f3f2bd
Aaron Krause <aaronjkrause@gmail.com>
2015-02-03 11:19:54 -0800

    added second bonus point

--- test/greetSpec.js
+++ test/greetSpec.js
@@ -1,17 +1,27 @@
 var expect = require('chai').expect;
 var app = require('../lib/greet');
+
 describe('Greet', function() {
   describe('#greet()', function() {
-    it('should admonish an empty input', function() {
+    it('should get a name from process.argv' , function() {
+      process.argv = ['node', 'somefile.js', 'Dave'];
+      var results = app.greet();
+
+      expect(results).to.equal('Hello, Dave I\'m Node.');
+
+      process.argv = {};
+    });
+
+    it('should admonish user for an empty input', function() {
       var results = app.greet();
 
       expect(results).to.contain('breh');
     });
+
     it('Should use the passed in name', function() {
       var results = app.greet('Dave');
 
       expect(results).to.equal('Hello, Dave I\'m Node.');
-
     });
 
   });
