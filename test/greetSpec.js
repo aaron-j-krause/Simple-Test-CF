c6c543fbf7aaec6bf33f37cb6b9a8e8421a8dae5
Aaron Krause <aaronjkrause@gmail.com>
2015-02-03 17:37:10 -0800

    last touchup I swear

--- test/greetSpec.js
+++ test/greetSpec.js
@@ -3,15 +3,6 @@ var app = require('../lib/greet');
 
 describe('Greet', function() {
   describe('#greet()', function() {
-    it('should get a name from process.argv' , function() {
-      var tempProc = process.argv;
-      process.argv = ['node', 'somefile.js', 'Dave'];
-      var results = app.greet();
-
-      expect(results).to.equal('Hello, Dave I\'m Node.');
-
-      process.argv = tempProc;
-    });
 
     it('should admonish user for an empty input', function() {
       var results = app.greet();
@@ -23,6 +14,23 @@ describe('Greet', function() {
       var results = app.greet('Dave');
 
       expect(results).to.equal('Hello, Dave I\'m Node.');
+    });
+    describe('should use process.argv', function() {
+      var args;
+      before(function() {
+        args = process.argv;
+        process.argv = ['node', 'somefile.js', 'Dave'];
+      });
+
+      after(function() {
+        process.argv = args;
+      });
+
+      it('should get a name from process.argv' , function() {
+        var results = app.greet();
+
+        expect(results).to.equal('Hello, Dave I\'m Node.');
+      });
     });
 
   });
